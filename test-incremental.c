4fb6f6ebaaeca0d2f24ba9188a67c7fecba844ca
Francesco Abbate <francesco.bbt@gmail.com>
2021-10-05 10:41:39 +0200

    Implement the dmon_watch_rm function for inotify
    The function dmon_watch_rm goes with dmon_watch_add to
    remove a subdirectory that was previously added.
    
    Like the function dmon_watch_add the function dmon_watch_rm
    is added only for inotify backend.

---
+++ test-incremental.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+#define DMON_IMPL
+#include "dmon.h"
+
+static void watch_callback(dmon_watch_id watch_id, dmon_action action, const char* rootdir,
+                           const char* filepath, const char* oldfilepath, void* user)
+{
+    (void)(user);
+    (void)(watch_id);
+
+    switch (action) {
+    case DMON_ACTION_CREATE:
+        printf("CREATE: [%s]%s\n", rootdir, filepath);
+        break;
+    case DMON_ACTION_DELETE:
+        printf("DELETE: [%s]%s\n", rootdir, filepath);
+        break;
+    case DMON_ACTION_MODIFY:
+        printf("MODIFY: [%s]%s\n", rootdir, filepath);
+        break;
+    case DMON_ACTION_MOVE:
+        printf("MOVE: [%s]%s -> [%s]%s\n", rootdir, oldfilepath, rootdir, filepath);
+        break;
+    }
+}
+
+static int prompt_command_loop(dmon_watch_id watch_id)
+{
+    char cmd[256];
+    while (1) {
+        fputs("> ", stdout);
+        char *s = fgets(cmd, 256, stdin);
+        char *newline = (s ? strchr(cmd, '\n') : NULL);
+        if (!s || !newline) {
+            fprintf(stderr, "error reading input\n");
+            return 1;
+        }
+        *newline = 0;
+        if (strncmp(cmd, "add ", 4) == 0) {
+            char *subdir = cmd + 4;
+            if (!dmon_watch_add(watch_id, subdir)) {
+                fprintf(stdout, "cannot add directory %s\n", subdir);
+            } else {
+                fprintf(stdout, "added directory %s\n", subdir);
+            }
+        } else if (strncmp(cmd, "remove ", 7) == 0) {
+            char *subdir = cmd + 7;
+            if (!dmon_watch_rm(watch_id, subdir)) {
+                fprintf(stdout, "cannot remove directory %s\n", subdir);
+            } else {
+                fprintf(stdout, "removed directory %s\n", subdir);
+            }
+        } else if (strcmp(cmd, "exit") == 0) {
+            return 0;
+        } else {
+            fprintf(stdout, "unknown command: %s\n", cmd);
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1) {
+        dmon_init();
+        puts("waiting for changes ..");
+        /* We do not watch recursively. */
+        dmon_watch_id watch_id = dmon_watch(argv[1], watch_callback, 0, NULL);
+        prompt_command_loop(watch_id);
+        dmon_deinit();
+    } else {
+        puts("usage: test dirname");
+    }
+    return 0;
+}
